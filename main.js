4e2a88d050cfd4c4c7331dfa838b2822b9b1b555
studstone <nikolas2996@gmail.com>
2022-03-25 20:56:51 +0300

    completed the second lesson

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-const title = 10;
-let screens = 10;
-let screenPrice = 10;
-let rollback = 10;
-let fullPrice = 10;
-let adaptive = 10;
+const title = 'Course by JavaScript';
+console.log(typeof title);
+let screens = 'Simple, complex, interactive';
+console.log('Переменная screens имеет длинну равную ' + screens.length);
+let screenPrice = 1000;
+console.log('Стоимость верстки экранов ' + screenPrice * 90 + ' рублей/ ' + screenPrice + ' долларов/ ' + screenPrice * 29.4 + ' гривен/ ' + screenPrice * 6.37 + ' юаней');
+let rollback = 34;
+let fullPrice = 500;
+console.log('Стоимость разработки сайта ' + fullPrice * 90 + ' рублей/ ' + fullPrice + ' долларов/ ' + fullPrice * 29.4 + ' гривен/ ' + fullPrice * 6.37 + ' юаней');
+console.log(typeof fullPrice);
+let adaptive = true;
+console.log(typeof adaptive);
 
-alert("Приветстыую на сайте");
-console.log("Приветствую в консоли");
+let lowerScreens = screens.toLowerCase();
+console.log(lowerScreens.split(", "));
+console.log('Процент отката посреднику равен ' + fullPrice * (rollback / 100) + ' долларов');
+//alert("Приветстыую на сайте");
+//console.log("Приветствую в консоли");
