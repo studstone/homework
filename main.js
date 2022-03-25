be5f17629f67d1999f1b10486283ed46908db8db
studstone <nikolas2996@gmail.com>
2022-03-25 21:04:54 +0300

    fix main.js

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ console.log(typeof title);
 let screens = 'Simple, complex, interactive';
 console.log('Переменная screens имеет длинну равную ' + screens.length);
 let screenPrice = 1000;
-console.log('Стоимость верстки экранов ' + screenPrice * 90 + ' рублей/ ' + screenPrice + ' долларов/ ' + screenPrice * 29.4 + ' гривен/ ' + screenPrice * 6.37 + ' юаней');
+console.log('Стоимость верстки экранов ' + screenPrice * 90 + ' рублей/ ' +
+  screenPrice + ' долларов/ ' + screenPrice * 29.4 + ' гривен/ ' + screenPrice * 6.37 + ' юаней');
 let rollback = 34;
 let fullPrice = 500;
-console.log('Стоимость разработки сайта ' + fullPrice * 90 + ' рублей/ ' + fullPrice + ' долларов/ ' + fullPrice * 29.4 + ' гривен/ ' + fullPrice * 6.37 + ' юаней');
+console.log('Стоимость разработки сайта ' + fullPrice * 90 + ' рублей/ ' +
+  fullPrice + ' долларов/ ' + fullPrice * 29.4 + ' гривен/ ' + fullPrice * 6.37 + ' юаней');
 console.log(typeof fullPrice);
 let adaptive = true;
 console.log(typeof adaptive);
