c356e510e08b1c7f1b996ded550a4b445e1c2fac
studstone <nikolas2996@gmail.com>
2022-03-28 20:45:59 +0300

    fix

--- main.js
+++ main.js
@@ -18,19 +18,14 @@ console.log(typeof adaptive);
 console.log(lowerScreens.split(", "));
 console.log('Процент отката посреднику равен ' + fullPrice * (rollback / 100) + ' долларов');
 
-let service1;
-let service2;
-let servicePrice1;
-let servicePrice2;
-
 title = prompt('Как называется ваш проект?', 'Название-сайта');
 screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 adaptive = prompt('Нужен ли адаптив на сайте?', 'Если нет, остаьте поле пустым');
-service1 = prompt('Какой дополнительный тип услуги нужен?');
-servicePrice1 = +prompt('Сколько это будет стоить?');
-service2 = prompt('Какой дополнительный тип услуги нужен?');
-servicePrice2 = +prompt('Сколько это будет стоить?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
 let rollbackPrice = fullPrice * (rollback / 100);
@@ -47,11 +42,11 @@ console.log(servicePrice2);
 console.log(fullPrice);
 console.log('Итоговая сумма ' + servicePercentPrice);
 
-if (fullPrice > 30000) {
+if (fullPrice >= 30000) {
   console.log('Даем скидку в 10%');
-} else if (15000 < fullPrice && fullPrice < 30000) {
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
   console.log('Даем скидку в 5%');
-} else if (0 < fullPrice && fullPrice < 15000) {
+} else if (fullPrice >= 0 && fullPrice < 15000) {
   console.log('Скидка не предусмотрена');
 } else if (fullPrice < 0) {
   console.log('Что то пошло не так');
