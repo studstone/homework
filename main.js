9bde587394c2da428f2e621425d79e52ee043242
studstone <nikolas2996@gmail.com>
2022-03-28 17:04:19 +0300

    completed lesson 3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const title = 'Course by JavaScript';
+'use strict';
+let title = 'Course by JavaScript';
 let screens = 'Simple, complex, interactive';
 let screenPrice = 1000;
 let rollback = 34;
@@ -16,3 +17,42 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(lowerScreens.split(", "));
 console.log('Процент отката посреднику равен ' + fullPrice * (rollback / 100) + ' долларов');
+
+let service1;
+let service2;
+let servicePrice1;
+let servicePrice2;
+
+title = prompt('Как называется ваш проект?', 'Название-сайта');
+screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+adaptive = prompt('Нужен ли адаптив на сайте?', 'Если нет, остаьте поле пустым');
+service1 = prompt('Какой дополнительный тип услуги нужен?');
+servicePrice1 = +prompt('Сколько это будет стоить?');
+service2 = prompt('Какой дополнительный тип услуги нужен?');
+servicePrice2 = +prompt('Сколько это будет стоить?');
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let rollbackPrice = fullPrice * (rollback / 100);
+let servicePercentPrice = Math.ceil(fullPrice - rollbackPrice);
+
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(!!adaptive);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(fullPrice);
+console.log('Итоговая сумма ' + servicePercentPrice);
+
+if (fullPrice > 30000) {
+  console.log('Даем скидку в 10%');
+} else if (15000 < fullPrice && fullPrice < 30000) {
+  console.log('Даем скидку в 5%');
+} else if (0 < fullPrice && fullPrice < 15000) {
+  console.log('Скидка не предусмотрена');
+} else if (fullPrice < 0) {
+  console.log('Что то пошло не так');
+}
