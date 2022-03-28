a0fd9af01eba40bdb6616d4005f68c1efe90dde7
studstone <nikolas2996@gmail.com>
2022-03-28 14:46:43 +0300

    fix main.js

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const title = 'Course by JavaScript';
-console.log(typeof title);
 let screens = 'Simple, complex, interactive';
-console.log('Переменная screens имеет длинну равную ' + screens.length);
 let screenPrice = 1000;
-console.log('Стоимость верстки экранов ' + screenPrice * 90 + ' рублей/ ' +
-  screenPrice + ' долларов/ ' + screenPrice * 29.4 + ' гривен/ ' + screenPrice * 6.37 + ' юаней');
 let rollback = 34;
 let fullPrice = 500;
+let adaptive = true;
+let lowerScreens = screens.toLowerCase();
+
+console.log(typeof title);
+console.log('Переменная screens имеет длинну равную ' + screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice * 90 + ' рублей/ ' +
+  screenPrice + ' долларов/ ' + screenPrice * 29.4 + ' гривен/ ' + screenPrice * 6.37 + ' юаней');
 console.log('Стоимость разработки сайта ' + fullPrice * 90 + ' рублей/ ' +
   fullPrice + ' долларов/ ' + fullPrice * 29.4 + ' гривен/ ' + fullPrice * 6.37 + ' юаней');
 console.log(typeof fullPrice);
-let adaptive = true;
 console.log(typeof adaptive);
-
-let lowerScreens = screens.toLowerCase();
 console.log(lowerScreens.split(", "));
 console.log('Процент отката посреднику равен ' + fullPrice * (rollback / 100) + ' долларов');
-//alert("Приветстыую на сайте");
-//console.log("Приветствую в консоли");
