bca034d73c110269db2ffbba0b0fa921c199dea4
studstone <nikolas2996@gmail.com>
2022-04-11 19:16:24 +0300

    add log

--- js/main.js
+++ js/main.js
@@ -1,28 +1,36 @@
 'use strict';
 
 const title = document.getElementsByTagName('h1')[0];
-const button = document.getElementsByClassName('handler_btn');
+const buttonFirst = document.getElementsByClassName('handler_btn')[0];
+const buttonSecond = document.getElementsByClassName('handler_btn')[1];
 const plusButton = document.querySelector('.screen-btn');
-const div = document.querySelectorAll('.other-items.percent');
-const div1 = document.querySelectorAll('.other-items.number');
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
 const input = document.querySelector('.main-controls__range>input');
 const span = document.querySelector('.main-controls__range>.range-value');
-const elemInput = document.getElementsByClassName('total-input');
-let divs = document.querySelectorAll('.screen');
+const total = document.getElementsByClassName('total-input')[0];
+const totalCount = document.getElementsByClassName('total-input')[1];
+const totalCountOther = document.getElementsByClassName('total-input')[2];
+const totalFullCount = document.getElementsByClassName('total-input')[3];
+const totalCountRollback = document.getElementsByClassName('total-input')[4];
+let divScreen = document.querySelectorAll('.screen');
 
 
 console.log(title);
-console.log(button[0], button[1]);
+console.log(buttonFirst);
+console.log(buttonSecond);
 console.log(plusButton);
-[].forEach.call(div, function (el) {
+[].forEach.call(otherItemsPercent, function (el) {
   console.log(el);
 });
-[].forEach.call(div1, function (el) {
+[].forEach.call(otherItemsNumber, function (el) {
   console.log(el);
 });
 console.log(input);
 console.log(span);
-[].forEach.call(elemInput, function (el) {
-  console.log(el);
-});
-console.log(divs);
+console.log(total);
+console.log(totalCount);
+console.log(totalCountOther);
+console.log(totalFullCount);
+console.log(totalCountRollback);
+console.log(divScreen);
